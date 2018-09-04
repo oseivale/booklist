54f4315a43a432fdfb4a233131ca807c3d95869c
Valerie Osei <valerie.osei@gmail.com>
2018-09-04 17:00:56 -0400

    added format responses in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,21 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
 
+    respond_to do |format|
+      format.html
+      format.text
+      format.json do
+        render json: @books.map {|book|
+          {title: book.title,
+            author: book.author
+          }
+        }
+      end
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+    end
+  end
 end
